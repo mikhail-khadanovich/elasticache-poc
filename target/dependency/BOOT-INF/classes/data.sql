6e50da7437f2e04dea89fd4b4be99cdabadde7a6
Mikhail Khadanovich <mkhadanovich@infoledger.com>
2022-10-14 17:06:30 +0300

    Updated k8s config

---
+++ target/dependency/BOOT-INF/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER (id, password, username) VALUES (1, 'password', 'user1');
+INSERT INTO USER (id, password, username) VALUES (2, 'password', 'user2');
+INSERT INTO USER (id, password, username) VALUES (3, 'password', 'user3');
+INSERT INTO USER (id, password, username) VALUES (4, 'password', 'user4');
+INSERT INTO USER (id, password, username) VALUES (5, 'password', 'user5');
